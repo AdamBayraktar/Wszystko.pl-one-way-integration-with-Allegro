914baf00e627123fd402197ff6e8004d67fc648b
Alemoz EB <adambayraktar@gmail.com>
2023-08-14 15:17:23 +0200

    Add file with custom error classes - custom_exceptions.py

---
+++ custom_exceptions.py
@@ -0,0 +1,17 @@
+class deviceCodeError(Exception):
+    pass
+
+class accessTokenError(Exception):
+    pass
+
+class returnPoliciesIdError(Exception):
+    pass
+
+class guaranteePoliciesIdError(Exception):
+    pass
+
+class shippingPoliciesTariffIdError(Exception):
+    pass
+
+class complaintPoliciesIdError(Exception):
+    pass
