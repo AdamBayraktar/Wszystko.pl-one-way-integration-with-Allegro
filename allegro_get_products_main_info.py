07652ecbbd91724e129c2825b274b7c44f479acf
Alemoz EB <adambayraktar@gmail.com>
2023-08-30 17:02:07 +0200

    Add code file responsible for getting main information of user's products - allegro_get_products_main_info.py

---
+++ allegro_get_products_main_info.py
@@ -0,0 +1,59 @@
+import requests
+from get_access_token_allegro import get_token
+
+requests.packages.urllib3.disable_warnings()
+
+# get all user's products
+def main():
+    access_token = get_token()
+    example = all_products_id(access_token)
+    print(example)
+    
+
+def get_products_main_info(access_token:str, lower_price:int=0, upper_price:int=1000, limit:int=1000) -> list[dict]:
+    """Get main information about your products. Maximum number of returned products is 1000. If you have more than 1000 products, you can send multiple requests, each with a different set of 'lower_price' and 'upper_price' arguments, in order to retrieve all of the products.
+
+    Args:
+        access_token (str): unique token issued by Allegro. Use get_access_token_allegro module
+        lower_price (int, optional): The minimum price of requested products. Defaults to 0.
+        upper_price (int, optional): The maximum price of requested products. Defaults to 1000.
+        limit (int, optional): The maximum number of returned products. Defaults to 1000.
+
+    Returns:
+        list: list of dictionaries that represents each product
+    """    
+    url = "https://api.allegro.pl/sale/offers"
+    headers = {
+        'Accept': 'application/vnd.allegro.public.v1+json',
+        'Authorization': f'Bearer {access_token}'
+    }
+    params = {
+        "limit": limit,
+        'sellingMode.price.amount.gte': lower_price,
+        'sellingMode.price.amount.lte': upper_price,
+    }
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()["offers"]
+
+
+# gets all user's products and return list of products id
+def all_products_id(access_token:str, price_range:list[int]=[9, 14, 17, 20]):
+    """Get all your shop's products ID. Per one request you can get up to 1000 products. If you have more than 1000, then use price_range argument to split the request in such manner to get all products.
+
+    Args:
+        access_token (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
+    all_products = []
+    # max limit products per request is 1000 therefore divide by price to several requests to get all products
+    # for price in price_range
+    all_products.extend(get_products_main_info(access_token, upper_price=10))
+    all_products.extend(get_products_main_info(access_token, lower_price=10.01, upper_price=18))
+    all_products.extend(get_products_main_info(access_token, lower_price=18.01))
+    return [product["id"] for product in all_products]
+    
+
+if __name__ == "__main__":
+    main()
