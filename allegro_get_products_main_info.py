15e6385a527f8e7ace62112ec72676c219e9d6b1
Alemoz EB <adambayraktar@gmail.com>
2023-09-04 16:30:51 +0200

    Add all_prodcts_id() docstring and refactor the function that the price range can be controled by its arguments - allegro_get_products_main_info.py

--- allegro_get_products_main_info.py
+++ allegro_get_products_main_info.py
@@ -35,23 +35,27 @@ def get_products_main_info(access_token:str, lower_price:int=0, upper_price:int=
     response = requests.get(url, headers=headers, params=params)
     return response.json()["offers"]
 
-
 # gets all user's products and return list of products id
-def all_products_id(access_token:str, price_range:list[int]=[9, 14, 17, 20]):
+def all_products_id(access_token:str, price_range:list[int]=[9, 14, 17, 20]) -> list:
     """Get all your shop's products ID. Per one request you can get up to 1000 products. If you have more than 1000, then use price_range argument to split the request in such manner to get all products.
 
     Args:
-        access_token (_type_): _description_
+        access_token (str): unique token issued by Allegro. Use get_access_token_allegro module
+        price_range (list[int], optional): List of prices that for each range unique request is send. Defaults to [9, 14, 17, 20].
 
     Returns:
-        _type_: _description_
-    """    
+        list: list of product's ID
+    """   
     all_products = []
-    # max limit products per request is 1000 therefore divide by price to several requests to get all products
+    # max limit products per request is 1000 therefore to get all products send requests for different range of prices
     # for price in price_range
-    all_products.extend(get_products_main_info(access_token, upper_price=10))
-    all_products.extend(get_products_main_info(access_token, lower_price=10.01, upper_price=18))
-    all_products.extend(get_products_main_info(access_token, lower_price=18.01))
+    for index,price in enumerate(price_range):
+        if index == 0:
+            all_products.extend(get_products_main_info(access_token, upper_price=price))
+        elif index == (len(price_range) - 1):
+            all_products.extend(get_products_main_info(access_token, lower_price=price))
+        else:
+            all_products.extend(get_products_main_info(access_token, lower_price=price_range[index - 1], upper_price=price))
     return [product["id"] for product in all_products]
     
 
