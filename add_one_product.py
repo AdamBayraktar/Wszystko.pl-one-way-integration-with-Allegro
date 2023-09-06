1e431cc7ac03f263812b192ac02e6abe0815805a
Alemoz EB <adambayraktar@gmail.com>
2023-09-06 14:05:20 +0200

    Add allegro access token, product params to body, fix some bugs - add_one_product.py

--- add_one_product.py
+++ add_one_product.py
@@ -1,5 +1,6 @@
 import requests 
 from get_access_token import get_access_token
+from get_access_token_allegro import get_token
 from get_complaint_policy_id import get_complaint_policies_ids
 from get_guarantee_ids import get_guarantee_policies_ids
 from get_return_policy_ids import get_return_policies_ids
@@ -11,13 +12,17 @@ URL = "https://wszystko.pl/api"
 
 def main():
     access_token = get_access_token()
+    allegro_access_token = get_token()
     complaint_police_id = get_complaint_policies_ids(access_token)[0]
     guarantee_police_id = get_guarantee_policies_ids(access_token)[0]
     return_police_id = get_return_policies_ids(access_token)[0]
     shipping_tariff_police_id = get_shipping_tariff_ids(access_token)[0]
-    allegro_products = all_product_detail(access_token)
+    # reduce to max 10 product, you are doing it for testing purpose
+    allegro_products, all_id = all_product_detail(allegro_access_token, get_fixed_number_product=[True,3])
     for product in allegro_products:
-        addProduct(access_token, complaint_police_id, guarantee_police_id, return_police_id, shipping_tariff_police_id, product)
+        print(product)
+        new_product = NewWszystkoplProduct(access_token, product)
+        addProduct(access_token, complaint_police_id, guarantee_police_id, return_police_id, shipping_tariff_police_id, new_product)
     
 def addProduct(access_token, complaint_police_id, guarantee_police_id, return_police_id, shipping_tariff_police_id, allegro_product: NewWszystkoplProduct):
     END_POINT = '/me/offers'
@@ -28,18 +33,36 @@ def addProduct(access_token, complaint_police_id, guarantee_police_id, return_po
     request_body = {
         "title": allegro_product.name,
         "price": allegro_product.price,
-        # get the wszystkopl equvilant category
         "categoryId": allegro_product.category_id,
-        # get img url from wszystko pl
         "gallery": allegro_product.images,
         "vatRate": "23%",
         # check all required parameters
-        # "parameters": [
-        #     {
-        #     "id": 0,
-        #     "value": "string"
-        #     }
-        # ],
+        "parameters": [
+            {
+            "id": 2,
+            "value": allegro_product.ean
+            },
+            # {
+            # "id": 5,
+            # "value": allegro_product.sku
+            # },
+            {
+            "id": 3,
+            "value": allegro_product.sku
+            },
+            # {
+            # "id": 8,
+            # "value": allegro_product.brand
+            # },
+            {
+            "id": 1,
+            "value": 5
+            },
+            {
+            "id": 2,
+            "value": allegro_product.ean
+            },
+        ],
         # check the scheme and img urls
         "description": allegro_product.description,
         "guaranteeId": guarantee_police_id,
@@ -52,7 +75,7 @@ def addProduct(access_token, complaint_police_id, guarantee_police_id, return_po
         "userQuantityLimit": 100,
         "isDraft": True, # change to false when the function will be ready
         # "isDraft": allegro_product.is_draft
-        "stockQuantity": 0,
+        "stockQuantity": allegro_product.stock,
         "showUnitPrice": False
         }
     r = requests.post(URL + END_POINT, headers=headers, json=request_body)
