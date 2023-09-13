d0271e31901414a7cd4e6c94e14dce782ac29278
Alemoz EB <adambayraktar@gmail.com>
2023-09-13 16:24:16 +0200

    Change save file location to the variables folder - mapping_create_file_with_all_wszystko_categories.py

--- mapping_create_file_with_all_wszystko_categories.py
+++ mapping_create_file_with_all_wszystko_categories.py
@@ -5,7 +5,7 @@ from get_all_categories_without_subcategories import get_all_categories_without_
 def main():
     access_token = get_access_token()
     categories = get_all_categories_without_subcategories(access_token)
-    write_it_as_variable(categories, "mapping_all_wszystko_categories.py", "all_wszystko_categories")
+    write_it_as_variable(categories, "variables/mapping_all_wszystko_categories.py", "all_wszystko_categories")
     
     
 def write_it_as_variable(file, file_name, variable_name):
