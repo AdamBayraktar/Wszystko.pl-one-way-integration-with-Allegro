01f4234f307ef4c4d91dfd14e9e560c08845c7da
Alemoz EB <adambayraktar@gmail.com>
2023-09-20 16:35:19 +0200

    Refactor the code, add more comments, add set category parameters function, expand set parameter functionality - add NewWszystkoplProduct.py

--- NewWszystkoplProduct.py
+++ NewWszystkoplProduct.py
@@ -1,9 +1,12 @@
-from variables.mapping_auto_template_finished import mapped_categories as my_mapping_template
 from get_image_url import add_image
+from variables.mapping_auto_template_finished_with_params import mapped_categories as my_mapping_template
 
+# improve the performance of the image search function
+# refactor the code
 
 class NewWszystkoplProduct:
     def __init__(self, wszystkopl_access_token, allegro_product):
+        self.allegro_product = allegro_product
         self.id = allegro_product['id']
         self.is_draft = False
         self.category_id = self.__get_category_id(my_mapping_template, allegro_product['category']['id'])
@@ -12,9 +15,9 @@ class NewWszystkoplProduct:
         # self.images = [img for img in self.old_images_to_new.values()]
         self.images = [list(img.values())[0] for img in self.old_images_to_new]
         self.description = self.__adjust_scheme_description(allegro_product["description"]['sections'])
-        self.ean = self.__set_parameter(allegro_product, "EAN (GTIN)")
-        self.sku = self.__set_parameter(allegro_product, "Kod producenta")
-        self.brand = self.__set_parameter(allegro_product, "Marka")
+        self.ean = self.__set_parameter("EAN (GTIN)")
+        self.sku = self.__set_parameter("Kod producenta")
+        self.brand = self.__set_parameter("Marka")
         self.externalReferences = [{ "id": self.id, "kind": "allegro" }]
         self.externalCategories = [{"source": "allegro", "breadcrumb": [{"id": allegro_product['category']['id']}]}]
         self.externalAttributes = self.add_allegro_params(allegro_product["productSet"][0]["product"]["parameters"])
@@ -22,6 +25,7 @@ class NewWszystkoplProduct:
         self.stock = int(allegro_product["stock"]["available"])
         self.status = allegro_product['publication']["status"]
         # check categories required params
+        self.parameters = self.__set_category_parameters(self.category)
         self.data = {
             "name": self.name,
             "description": self.description,
@@ -39,13 +43,20 @@ class NewWszystkoplProduct:
             "status": "active" if self.status.lower() == "active" else "inactive"
         }
 
-    def __set_parameter(self, product, name):
-        parameters = product["productSet"][0]["product"]["parameters"]
+    def __set_parameter(self, name, the_type='int'):
+        parameters = self.allegro_product["productSet"][0]["product"]["parameters"]
         for param in parameters:
-            if param.get("name") == name:
+            if param.get("name").lower() == name.lower():
                 return param["values"][0]
-            else:
-                # some products might not have EAN
+        match the_type:
+            # some products might not have EAN
+            case 'integer':
+                return  10
+            case 'float':
+                return 100.11
+            case 'string':
+                return self.sku
+            case _:
                 return 5901801109167 # random EAN
             
 
@@ -94,21 +105,62 @@ class NewWszystkoplProduct:
         return new_description
 
     def __get_category_id(self, mapping_template:list[dict], allegro_category_id: str) -> int:
+        """Return the category ID and create category property that hold information about the category.
+        If failed to find then set product as a draft and create category property as False
+
+        Args:
+            mapping_template (list[dict]): the list of mapped categories
+            allegro_category_id (str): the allegro category ID
+
+        Returns:
+            int: wszystko_pl category id
+            create: self.category
+        """        
         for category in mapping_template:
             if category['id'] == allegro_category_id:
+                self.category = category
                 return int(category['wszystko_id'])
         # if the category is not found set product as a draft and return default category which is 0
         self.__set_as_draft()
+        self.category = False
         return 0
 
     def __set_as_draft(self) -> None:
         self.is_draft = True
             
-
-
-
-
-
-        
+    
+    def __set_category_parameters(self, category: list) -> list:
+        # if category argument is False then return only default parameters
+        parameters = [
+            {
+            "id": 2,
+            "value": self.ean
+            },
+            {
+            "id": 3,
+            "value": self.sku
+            },
+            {
+            "id": 1,
+            "value": 5
+            }
+        ]
+        if not category:
+            return parameters
+        for the_parameter in category["parameters"]:
+            # add parameters with ready values
+            if the_parameter.get('ready_value', False):
+                parameters.append({
+                    "id": the_parameter['id'],
+                    "value": the_parameter['ready_value']
+                })
+            # add value to parameters that do not have the value
+            elif the_parameter.get('extra', False):
+                parameters.append({
+                    "id": the_parameter['id'],
+                    "value": self.__set_parameter(the_parameter['extra'], the_type=the_parameter['type']) 
+                })
+        return parameters
+#SKU Marka moc pojemnosc
 
 
