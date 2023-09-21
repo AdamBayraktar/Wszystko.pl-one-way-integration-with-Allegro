f32d0e8c63c243d1debc5583564f4b64c20ee517
Alemoz EB <adambayraktar@gmail.com>
2023-09-21 15:00:26 +0200

    import traceback, add list that have record of products IDs for both platform, create functionality for that and save it to file, add list that have record of failed to add products, create functionality for it and save it as variable, add functionality that wont crash when exception occurs, fix product status error - add_one_product.py

--- add_one_product.py
+++ add_one_product.py
@@ -1,4 +1,5 @@
 import requests 
+import traceback
 from get_access_token import get_access_token
 from get_access_token_allegro import get_token
 from get_complaint_policy_id import get_complaint_policies_ids
@@ -9,7 +10,10 @@ from NewWszystkoplProduct import NewWszystkoplProduct
 from allegro_get_product_details import all_product_detail
 
 URL = "https://wszystko.pl/api"
+created_products = []
+failed_to_create_products = []
 
+# Change file to add just one product like it's name states
 def main():
     access_token = get_access_token()
     allegro_access_token = get_token()
@@ -18,14 +22,23 @@ def main():
     return_police_id = get_return_policies_ids(access_token)[0]
     shipping_tariff_police_id = get_shipping_tariff_ids(access_token)[0]
     # reduce to max [MAX_PRODUCT_NUMBER] products, you are doing it for testing purpose
-    MAX_PRODUCT_NUMBER = 10
+    MAX_PRODUCT_NUMBER = 4
     allegro_products, all_id = all_product_detail(allegro_access_token, get_fixed_number_product=[True,MAX_PRODUCT_NUMBER])
     total_products_number = len(allegro_products)
     for actual_product_number, product in enumerate(allegro_products):
         actual_product_number += 1
-        new_product = NewWszystkoplProduct(access_token, product)
         print(f"Progression: {actual_product_number}/{total_products_number}")
-        addProduct(access_token, complaint_police_id, guarantee_police_id, return_police_id, shipping_tariff_police_id, new_product)
+        try:
+            new_product = NewWszystkoplProduct(access_token, product)
+        except Exception:
+            print("Failed to create a product to add")
+            print(f"An exception occurred:\n{Exception}")
+            traceback.print_exc()
+            print(product)
+        else:
+            addProduct(access_token, complaint_police_id, guarantee_police_id, return_police_id, shipping_tariff_police_id, new_product)
+    write_it_as_variable(created_products, 'variables/active_products.py', 'active_products')
+    write_it_as_variable(failed_to_create_products, 'variables/failed_to_add_products.py', 'failed_to_add_products')
     
 def addProduct(access_token, complaint_police_id, guarantee_police_id, return_police_id, shipping_tariff_police_id, allegro_product: NewWszystkoplProduct):
     END_POINT = '/me/offers'
@@ -47,7 +60,7 @@ def addProduct(access_token, complaint_police_id, guarantee_police_id, return_po
         "shippingTariffId": shipping_tariff_police_id,
         "leadTime": "Natychmiast",
         "stockQuantityUnit": "sztuk",
-        "status": "active",
+        "status": allegro_product.status,
         "userQuantityLimit": 100,
         "isDraft": allegro_product.is_draft,
         "stockQuantity": allegro_product.stock,
@@ -61,9 +74,16 @@ def addProduct(access_token, complaint_police_id, guarantee_police_id, return_po
     if r.status_code == 200:
         # print(f"{r}\\{r.text}")
         print("Successfully added")
+        created_products.append({'wszystko_product_id': r.json()['id'], 'allegro_id': allegro_product.id})
     else:
         print(send_parameters)
         print(f"Something went wrong while adding the product. Check the message:\n{r}\n{r.text}")
+        failed_to_create_products.append(f"{len(failed_to_create_products)}. product failed to add, the message:\n{r}\n{r.text}\n\n\n")
+
+ 
+def write_it_as_variable(file, file_name, variable_name):
+    with open(file_name, 'w', encoding='utf-8') as f:
+        f.write(f'{variable_name} = {file}')
 
     
 if __name__ == "__main__":
